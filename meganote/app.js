35fee5beccd1fb6a668582095c5dbb432b0ae8a1
kushp44 <kushp44@gmail.com>
2016-06-14 09:41:06 -0400

    6/14 push

---
+++ meganote/app.js
@@ -0,0 +1,13 @@
+(function() {
+  var app = angular.module('meganote', [
+    'ui.router',
+    'meganote.notes'
+  ]);
+
+  function config($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/notes');
+  }
+
+  config.$inject = ['$urlRouterProvider'];
+  app.config(config);
+})();
