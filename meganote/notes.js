937b4b12b04cfbb4b8cef8ab192d1225e435f383
kushp44 <kushp44@gmail.com>
2016-06-14 13:26:41 -0400

    notes fix

---
+++ meganote/notes.js
@@ -0,0 +1,21 @@
+angular.module('meganote.notes', [
+   'ui.router'
+ ])
+
+ .config(function($stateProvider) {
+   $stateProvider
+
+   .state('notes', {
+       url: '/notes',
+       templateUrl: 'notes/notes.html',
+       controller: 'NotesController'
+     })
+
+   .state('notes.form', {
+     url: '/:noteId',
+     templateUrl: 'notes/notes-form.html'
+    });
+  })
+
+  .controller('NotesController', function() {
+  });
